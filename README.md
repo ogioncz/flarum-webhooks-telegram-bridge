578813148095ffb6cb87f78bcb5e57b3567e809f
Jan Tojnar <jtojnar@gmail.com>
2021-06-13 14:18:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# flarum-webhooks-telegram-bridge
+
+This is a bridge between Flarum [Webhooks](https://github.com/FriendsOfFlarum/webhooks) extension and Telegram.
+
+## How to use
+
+1. Follow the [instructions](https://github.com/php-telegram-bot/core/blob/master/README.md#create-your-first-bot) to create a Telegram bot.
+2. Invite the bot to the room where you want it to post and [obtain the room id](https://stackoverflow.com/a/32572159/160386).
+3. Clone this repository somewhere (e.g. into your Flarum installation’s `public/` directory).
+4. Run `composer install` in the directory to install dependencies.
+5. Create `config.php` file in the directory, containing:
+
+    ```php
+    <?php
+
+    return [
+        'apiKey' => 'your-api-key',
+        'botName' => 'your-bot-name',
+        'chatId' => 'your-chat-id',
+        'token' => 'your-random-string',
+    ];
+    ```
+6. Replace `your-bot-name` with the bot’s username, `your-api-key` with the “token to access the HTTP API” you received from BotFather, `your-chat-id` with the id of the room id and `your-random-string` with random string.
+7. In the “FoF Webhooks” section of the Flarum admin panel add a webhook for “Slack” and set the URL to the location where you installed this project and pass `action=hook` and `token=your-random-string` in the query string (e.g. `https://your-flarum-forum.com/flarum-webhooks-telegram-bridge/?action=hook&token=your-random-string`)
+
+## Related projects
+
+- [Telegram support for flarum-ext-notify](https://github.com/manelizzard/flarum-notify/pull/6)
+
+## License
+
+Please see the LICENSE included in this repository for a full copy of the MIT license, which this project is licensed under.
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Telegram;
+
+/**
+ * Method which actually sends a message to Telegram
+ * @param  array $message
+ * @return void
+ */
+function send($message) {
+	$message = $message['attachments'][0];
+	$content = '<a href="' . htmlspecialchars($message['title_link'], ENT_QUOTES) . '">' . $message['title'] . '</a>: ' . $message['fallback'];
+
+	$data = createMessagePayload($content);
+
+	$result = Request::sendMessage($data);
+}
+
+/**
+ * Checks wether the Connector works with the current settings
+ * @return boolean
+ */
+function works() {
+	$data = createMessagePayload('Flarum is able to contact this Telegram room. Great!');
+
+	try {
+		$result = Request::sendMessage($data);
+
+		return $result->isOk();
+	} catch (TelegramException $e) {
+		return false;
+	}
+}
+
+/**
+ * Creates a payload to be sent using the API.
+ * @param string $message
+ * @return array
+ */
+function createMessagePayload($message) {
+	global $config;
+	return [
+		'chat_id' => $config['chatId'],
+		'text' => $message,
+		'parse_mode' => 'HTML',
+	];
+}
+
+
+$config = require __DIR__ . '/config.php';
+
+if (!isset($_GET['token']) || $_GET['token'] !== $config['token']) {
+	http_response_code(403);
+	echo 'Invalid webhook token' . PHP_EOL;
+	die;
+}
+
+if (isset($_GET['action'])) {
+	$telegram = new Telegram($config['apiKey'], $config['botName']);
+
+	if ($_GET['action'] === 'test') {
+		works();
+		die;
+	} elseif ($_GET['action'] === 'hook') {
+		$json = file_get_contents('php://input');
+		$message = json_decode($json, TRUE);
+		send($message);
+		die;
+	}
+}
+
+http_response_code(400);
+echo 'Unknown action' . PHP_EOL;
